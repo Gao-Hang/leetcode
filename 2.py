f49026fbbfc5e35460ee157924b16091fd4d75fc
Gao-Hang <44079444+Gao-Hang@users.noreply.github.com>
2019-03-15 14:54:22 +0800

    Create 2.py

---
+++ 2.py
@@ -0,0 +1,90 @@
+class ListNode:
+    def __init__(self, val=None, next=None):
+        self.val = val
+        self.next = next
+
+
+class List_Node:
+    def list_node(self, array):
+        tem_node = ListNode()
+        node = ListNode()
+        for i in array:
+            # 记得是判定val是否有值，并且用一个node记住头节点，然后返回的是头节点
+            if not tem_node.val:
+                tem_node.val = i
+                node = tem_node
+            else:
+                tem_node.next = ListNode(i)
+                tem_node = tem_node.next
+        return node
+
+
+class Solution:
+    def add_numbers(self, l1, l2):
+        """
+        :type l1: ListNode
+        :type l2: ListNode
+        :rtype: ListNode
+        """
+        re = ListNode(0)
+        r = re
+        carry = 0
+        while l1 or l2:
+            x = l1.val if l1 else 0
+            y = l2.val if l2 else 0
+            s = carry + x + y
+            carry = s // 10
+            r.next = ListNode(s % 10)
+            r = r.next
+            if l1 is not None:
+                l1 = l1.next
+            if l2 is not None:
+                l2 = l2.next
+        if carry > 0:
+            r.next = ListNode(1)
+        return re.next
+
+
+class Solution2:
+    # @return a ListNode
+    def addTwoNumbers(self, l1, l2):
+        carry = 0
+        root = n = ListNode(0)
+        while l1 or l2 or carry:
+            v1 = v2 = 0
+            if l1:
+                v1 = l1.val
+                l1 = l1.next
+            if l2:
+                v2 = l2.val
+                l2 = l2.next
+            carry, val = divmod(v1+v2+carry, 10)
+            n.next = ListNode(val)
+            n = n.next
+        return root.next
+
+
+class Solution3:
+    def addTwoNumbers(self, l1, l2):
+        dummy = cur = ListNode(0)
+        carry = 0
+        while l1 or l2 or carry:
+            if l1:
+                carry += l1.val
+                l1 = l1.next
+            if l2:
+                carry += l2.val
+                l2 = l2.next
+            cur.next = ListNode(carry%10)
+            cur = cur.next
+            carry //= 10
+        return dummy.next
+
+
+if __name__ == '__main__':
+    l1 = [2, 4, 3]
+    l2 = [5, 6, 4]
+    l1 = List_Node().list_node(l1)
+    l2 = List_Node().list_node(l2)
+    a = Solution().add_numbers(l1, l2)
+    print(a)
